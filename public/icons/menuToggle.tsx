5fc032c6bf85ae250f55f173ad64f0b9dd02df0e
joel danjuma <leojjad@gmail.com>
2024-01-29 22:31:23 +0100

    Initial Commit

---
+++ public/icons/menuToggle.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { IconSvgProps } from "@/types/typings";
+
+export const MenuToggle : React.FC<IconSvgProps> = () => (
+    <svg width="52" height="52" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="52" height="52" rx="15" fill="white"/>
+<circle cx="5" cy="5" r="5" transform="matrix(-1 0 0 1 36.5 15)" fill="black"/>
+<circle cx="5" cy="5" r="5" transform="matrix(-1 0 0 1 24.5 15)" fill="black"/>
+<circle cx="5" cy="5" r="5" transform="matrix(-1 0 0 1 36.5 27)" fill="black"/>
+<circle cx="5" cy="5" r="5" transform="matrix(-1 0 0 1 24.5 27)" fill="black"/>
+</svg>
+
+)
