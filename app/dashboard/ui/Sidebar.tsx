191e7363db7d90f516b6a62ba05f880da11d61fe
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2024-03-27 19:38:55 +0000

    Design dashboard sidebar

--- app/dashboard/ui/Sidebar.tsx
+++ app/dashboard/ui/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import logo from "@/public/logo.svg";
 import Image from "next/image";
 import { HomeIcon } from "@/public/icons/home";
@@ -5,6 +7,8 @@ import { FeedbackIcon } from "@/public/icons/feedback";
 import { UserIcon } from "@/public/icons/user";
 import { CompassIcon } from "@/public/icons/compass";
 import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 type NavLink = {
   title: string;
@@ -13,13 +17,14 @@ type NavLink = {
 };
 
 const navLinks: NavLink[] = [
-  { title: "Home", icon: HomeIcon, href: "" },
+  { title: "Home", icon: HomeIcon, href: "/dashboard" },
   { title: "Explore", icon: CompassIcon, href: "" },
   { title: "Feedback", icon: FeedbackIcon, href: "" },
   { title: "Profile", icon: UserIcon, href: "" },
 ];
 
 function SideNav() {
+  const pathName = usePathname();
   return (
     <nav className="border-1 border-white bg-white/[4%] h-full lg:flex flex-col items-start text-white py-4 rounded-[10px]">
       <Image
@@ -30,11 +35,17 @@ function SideNav() {
         className="mb-28 mx-auto"
       />
 
-      <div className="flex flex-col gap-3 bg-red-700 w-full">
+      <div className="flex flex-col gap-[18px] w-full">
         {navLinks.map((navLink) => (
           <Link
             href={navLink.href}
-            className="bg-emerald-700 ps-7 flex gap-3 items-center h-[35px] transition-colors"
+            className={clsx(
+              "ms-7 flex gap-3 items-center transition-colors py-[5px] px-[7px] rounded-s-[10px]",
+              {
+                "bg-gradient-to-r from-[#CC00E3] from-2% to-[#70007D]/[54%]":
+                  navLink.href == pathName,
+              }
+            )}
           >
             <navLink.icon />
             {navLink.title}
