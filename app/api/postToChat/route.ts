0d73f9c8f1cd6ee70eec8aa4404367fbb1284c09
joel-danjuma <leojjad@gmail.com>
2024-04-05 02:02:20 +0100

    Another updaetest route for chat feature

---
+++ app/api/postToChat/route.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import crypto from "crypto";
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const { input } = req.body;
+
+  const random = crypto.randomBytes(32).toString("hex"),
+    hash = crypto
+      .createHmac("sha256", process.env.LINGOLETTE_AUTH_SECRET || "")
+      .update(random)
+      .digest("hex");
+
+  const response = await fetch("https://lingolette.com/api/binary", {
+    method: "POST",
+    // cache: "no-cache",
+    // keepalive: true,
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-version": "1",
+      "x-random": random,
+      "x-auth-id": process.env.LINGOLETTE_AUTH_ID || "",
+      "x-auth-key": hash,
+    },
+    body: JSON.stringify({
+      method: "postToChat",
+      data: {
+        input: input,
+        useVoiceOut: false,
+      },
+    }),
+  });
+
+  if (!response.ok) {
+    // res.status(response.status).json({ error: "Failed to fetch data" });
+    return new Response("Failed to fetch data", {
+      status: response.status,
+    });
+  }
+
+  const stream = response.body;
+
+  if (!stream) {
+    // Handle the case where stream is null, e.g., by returning an error response
+    return new Response("Stream is null", {
+      status: response.status,
+    });
+  }
+  return new StreamingTextResponse(stream);
+}
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,123 @@
+// "use client";
+// import { useEffect, useState } from "react";
+// import crypto from "crypto";
+// import { SSE } from "sse.js";
+
+// interface Message {
+//   role: string;
+//   message: any;
+// }
+
+// const Page = () => {
+//   const [messages, setMessages] = useState<Message[]>([]);
+//   const [input, setInput] = useState("");
+//   const [eventSource, setEventSource] = useState<EventSource | null>(null);
+//   const random = crypto.randomBytes(32).toString("hex"),
+//     hash = crypto
+//       .createHmac("sha256", process.env.LINGOLETTE_API_SECRET || "")
+//       .update(random)
+//       .digest("hex");
+
+//   const startChat = () => {
+//     console.log("Starting chat..."); // Signal that the chat is starting
+//     // No need to reassign EventSource here, just use it directly
+//     const es = new EventSource("https://localhost:3000/api/startChat");
+//     // setEventSource(es);
+
+//     es.onopen = () => {
+//       console.log("Chat started successfully."); // Signal that the chat has started successfully
+//       // You might want to send an initial message or perform other setup tasks here
+//     };
+
+//     es.onmessage = (event) => {
+//       const data = JSON.parse(event.data);
+//       setMessages((prevMessages) => [
+//         ...prevMessages,
+//         { role: "ai", message: data[0] },
+//       ]);
+//     };
+//     es.onerror = (error) => {
+//       console.error("EventSource error:", error);
+//     };
+//   };
+
+//   const handleSubmit = () => {
+//     const es = new EventSource("https://localhost:3000/api/startChat");
+//     // setEventSource(es);
+//     if (es) {
+//       es.onopen = (e) => {
+//         console.log("Chat started successfully."); // Signal that the chat has started successfully
+//       };
+//       setMessages((prevMessages) => [
+//         ...prevMessages,
+//         { role: "user", message: input },
+//       ]);
+//       setInput("");
+//       es.onmessage = (event) => {
+//         const data = JSON.parse(event.data);
+//         setMessages((prevMessages) => [
+//           ...prevMessages,
+//           { role: "ai", message: data[0] },
+//         ]);
+//       };
+//       es.onerror = (error) => {
+//         console.error("EventSource error:", error);
+//       };
+//     } else {
+//       console.error("EventSource is not initialized.");
+//     }
+//   };
+
+//   //   useEffect(() => {
+//   //     return () => {
+//   //       if (eventSource) {
+//   //         eventSource.close();
+//   //       }
+//   //     };
+//   //   }, [eventSource]);
+
+//   return (
+//     <div className="w-full min-h-screen flex flex-col items-center justify-center">
+//       <div className="flex-col mb-4 min-h-[420px] max-w-[500px] min-w-[420px] border-2 rounded-lg p-2 ">
+//         {messages.map((message, index) => (
+//           <p
+//             key={index}
+//             className={
+//               message.role === "user"
+//                 ? "text-blue-500 text-2xl"
+//                 : "text-green-500 text2-2xl"
+//             }
+//           >
+//             {message.message}
+//           </p>
+//         ))}
+//       </div>
+//       <div>
+//         <input
+//           type="text"
+//           value={input}
+//           onChange={(e) => setInput(e.target.value)}
+//           className="border-2 border-gray-300 p-2 rounded-md"
+//         />
+//         <button
+//           onClick={handleSubmit}
+//           className="bg-blue-500 text-white p-2 rounded-md ml-2"
+//         >
+//           Send
+//         </button>
+//         <button
+//           onClick={startChat}
+//           className="bg-green-500 text-white p-2 rounded-md ml-2"
+//         >
+//           Start Chat
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
 "use client";
 import { useEffect, useState } from "react";
 import crypto from "crypto";
@@ -13,10 +133,7 @@ const Page = () => {
   const [eventSource, setEventSource] = useState<EventSource | null>(null);
   const random = crypto.randomBytes(32).toString("hex"),
     hash = crypto
-      .createHmac(
-        "sha256",
-        "6f6f9f2383820e432fdcb233083f38b0b4019478cb598c6df344225dcfa7224f"
-      )
+      .createHmac("sha256", process.env.LINGOLETTE_AUTH_SECRET || "")
       .update(random)
       .digest("hex");
 
@@ -27,19 +144,19 @@ const Page = () => {
 
     es.onopen = () => {
       console.log("Chat started successfully."); // Signal that the chat has started successfully
-      fetch("https://lingolette.com/api/binary/", {
+      fetch("https://lingolette.com/api/binary", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "x-api-version": "1",
           "x-random": random,
-          "x-auth-id": "5ae65ce4-e04d-11ee-8617-7f4613917414",
+          "x-auth-id": process.env.LINGOLETTE_AUTH_ID || "",
           "x-auth-key": hash,
         },
         body: JSON.stringify({
           method: "startChat",
           data: {
-            timeStamp: new Date().toISOString(),
+            timeStamp: new Date(),
             useVoiceOut: false,
           },
         }),
@@ -65,13 +182,13 @@ const Page = () => {
     if (eventSource) {
       eventSource.onopen = () => {
         console.log("Chat started successfully."); // Signal that the chat has started successfully
-        fetch("https://lingolette.com/api/binary/", {
+        fetch("https://lingolette.com/api/binary", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
             "x-api-version": "1",
             "x-random": random,
-            "x-auth-id": "5ae65ce4-e04d-11ee-8617-7f4613917414",
+            "x-auth-id": process.env.LINGOLETTE_AUTH_ID || "",
             "x-auth-key": hash,
           },
           body: JSON.stringify({
