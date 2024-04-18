499c3034be95b79ea56a595443a14d2a4b39a995
Owusu Kelvin Clark <kelvinclark@lokaitech.com>
2024-04-18 08:55:23 +0100

    Implement update login [Partial]

--- app/actions/auth/profile.ts
+++ app/actions/auth/profile.ts
@@ -1,7 +1,33 @@
-"use server"
+"use server";
+import { createClient } from "@/utils/supabase/server";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-import { createClient } from "@/utils/supabase/server"
+async function updateProfile(formdata: FormData) {
+  const supabase = createClient();
+  const {
+    error,
+    data: { user },
+  } = await supabase.auth.getUser();
 
-function updateProfile(formdata: FormData) {
-    const supabase = createClient();
+  if (error || user === null) {
+    redirect("/login");
+  }
+
+  const userProfile = await db.userProfile.findUnique({where: {id: user.id}, include: {LingoletteCredential: true}})
+
+  if (userProfile === null) {
+    redirect("/login")
+  }
+
+  // perform proper sanitization 
+  const data = {
+    name: formdata.get("name") ?? userProfile.name,
+    nativeLng: formdata.get("native-language") ?? userProfile.LingoletteCredential?.nativeLng,
+    targetLng: formdata.get("language-studying") ?? userProfile.LingoletteCredential?.targetLng,
+    langLevel: formdata.get("level") ?? userProfile.LingoletteCredential?.languageLevel,
+    grammaticalGender: formdata.get("grammatical-gender") ?? userProfile.grammatical_gender
+  }
+
+  // check if lingolette table properties are changed
 }
