489e37b003bd7d010d58bedd146ec0bedaf1237b
joel-danjuma <leojjad@gmail.com>
2024-04-24 23:34:18 +0100

    remove skeleton for ai message

--- app/dashboard/explore/components/ChatSection.tsx
+++ app/dashboard/explore/components/ChatSection.tsx
@@ -12,17 +12,17 @@ function ChatSection({
     <>
       {messages.map((msg) => (
         <div key={msg.id} className="grid grid-cols-[auto_1fr] gap-2 my-3">
-          {msg.role === "user" ? <Avatar /> : <Avatar name="DwnAI" />}
+          {msg.role === "user" ? <Avatar /> : <Avatar name="AIDA" />}
           <p className="rounded-md h-max py-1 px-3">{msg.content}</p>
         </div>
       ))}
-      {isLoading && (
+      {/* {isLoading && (
         <div className="flex flex-col gap-1 py-2 pl-2">
           <Skeleton className="h-[10px] max-w-[40%] rounded-lg" />
           <Skeleton className="h-[10px] max-w-[30%] rounded-lg" />
           <Skeleton className="h-[10px] max-w-[20%] rounded-lg" />
         </div>
-      )}
+      )} */}
     </>
   );
 }
