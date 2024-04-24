eadc51efe8bb09101e9269b34c019f8a0db11dbe
joel-danjuma <leojjad@gmail.com>
2024-04-24 22:46:54 +0100

    Completion of chat feature on explore seperation of chat input and chat section into seperate components minor update to signUpCard for deployment error

--- app/dashboard/explore/components/ChatInput.tsx
+++ app/dashboard/explore/components/ChatInput.tsx
@@ -1,44 +1,75 @@
-import { AttachmentIcon } from "@/public/icons/Attachment";
-import { MicIcon } from "@/public/icons/Mic";
-import { SendIcon } from "@/public/icons/Send";
-import { Input } from "@nextui-org/react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Mic, Paperclip, CornerDownLeft } from "lucide-react";
+
+interface ChatInputProps {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleInputChange: (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => void;
+  input: string;
+}
 
 function ChatInput({
-  handleSendChat,
-  setInput,
-  disableInputs,
-  input
-}: {
-  handleSendChat: () => void;
-  setInput: (input: string) => void;
-  disableInputs: boolean;
-  input: string
-}) {
+  handleSubmit,
+  handleInputChange,
+  // disableInputs,
+  input,
+}: ChatInputProps) {
   return (
     <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSendChat();
-      }}
-      className="flex items-center gap-7 bg-[#0F0F0F] border-1 border-[#0F0F0F]/[10%] rounded-[13px] py-2 ps-12 pe-7 mt-20"
+      onSubmit={handleSubmit}
+      className="absolute bottom-0 w-full max-h-[20vh] overflow-hidden rounded-lg border bg-background  "
+      x-chunk="dashboard-03-chunk-1"
     >
-      <AttachmentIcon />
-      <Input
-        type="text"
-        placeholder="Type your message"
-        disabled={disableInputs}
+      <Label htmlFor="message" className="sr-only">
+        Message
+      </Label>
+      <Textarea
+        id="message"
         value={input}
-        onValueChange={setInput}
+        onChange={handleInputChange}
+        placeholder="Type your message here..."
+        className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
       />
-      <div className="flex items-center gap-4">
-        <MicIcon />
-        <button
-          className="bg-[#A124B6] w-[47px] h-[47px] rounded-[50%] grid place-content-center"
-          disabled={disableInputs}
-          type="submit"
-        >
-          <SendIcon />
-        </button>
+      <div className="flex items-center p-3 pt-0">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Paperclip className="size-4" />
+                <span className="sr-only">Attach file</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top">Attach File</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Mic className="size-4" />
+                <span className="sr-only">Use Microphone</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="top">Use Microphone</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <Button type="submit" size="sm" className="ml-auto gap-1.5">
+          Send Message
+          <CornerDownLeft className="size-3.5" />
+        </Button>
       </div>
     </form>
   );
--- app/dashboard/explore/components/ChatSection.tsx
+++ app/dashboard/explore/components/ChatSection.tsx
@@ -1,24 +1,23 @@
 import { Avatar, Skeleton } from "@nextui-org/react";
-import { Message } from "../page";
-import clsx from "clsx";
+import { Message } from "ai";
 
 function ChatSection({
   messages,
-  isGettingAIResponse,
+  isLoading,
 }: {
   messages: Message[];
-  isGettingAIResponse: boolean;
+  isLoading: boolean;
 }) {
   return (
     <>
-      {messages.map((msg, idx) => (
-        <div key={idx} className="grid grid-cols-[auto_1fr] gap-2 my-3">
+      {messages.map((msg) => (
+        <div key={msg.id} className="grid grid-cols-[auto_1fr] gap-2 my-3">
           {msg.role === "user" ? <Avatar /> : <Avatar name="DwnAI" />}
-          <p className="rounded-md h-max py-1 px-3">{msg.message}</p>
+          <p className="rounded-md h-max py-1 px-3">{msg.content}</p>
         </div>
       ))}
-      {isGettingAIResponse && (
-        <div className="flex flex-col gap-1">
+      {isLoading && (
+        <div className="flex flex-col gap-1 py-2 pl-2">
           <Skeleton className="h-[10px] max-w-[40%] rounded-lg" />
           <Skeleton className="h-[10px] max-w-[30%] rounded-lg" />
           <Skeleton className="h-[10px] max-w-[20%] rounded-lg" />
