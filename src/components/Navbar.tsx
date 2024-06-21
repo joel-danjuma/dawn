5f081369c3cc30b799fde16d708d65ce106085d2
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-06-22 00:21:34 +0100

    mobile teachers/ai tools

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,44 +3,70 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-            <a className="mr-5 hover:text-gray-900">First Link</a>
-            <a className="mr-5 hover:text-gray-900">Second Link</a>
-            <a className="mr-5 hover:text-gray-900">Third Link</a>
-            <a className="hover:text-gray-900">Fourth Link</a>
-          </nav>
-          <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
+      <header className="text-white p-5  ">
+        <div className="flex justify-between flex-row">
+          <div>
+            <img src="/harmburger.png" className="" alt="" />
+          </div>
+
+          <a className="">
+            <span className="block">
+              <img src="/dawn-logo.png" className="" alt="" />
+            </span>
           </a>
-          <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-            <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-              Button
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-4 h-4 ml-1"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </button>
+
+          <div className="flex gap-3 justify-center items-center">
+            <span className="bg-red-600 h-2 w-2 rounded-lg"></span>
+            <span className="bg-blue-600 h-2 w-2 rounded-lg"></span>
+            <span className="bg-green-600 h-2 w-2 rounded-lg"></span>
+          </div>
+        </div>
+
+        <div className="bg-[#0c0e12] flex mt-10 text-center  rounded-full shadow-sm container ">
+          <span className="block bg-transparent w-full p-3 rounded-full">
+            Dawn School
+          </span>
+          <span className="block bg-[#B91381] w-full p-3 rounded-full">
+            Dawn Students
+          </span>
+        </div>
+
+        <div className="bg-[#0c0e12] mt-5 rounded-md">
+          <input type="search" />
+        </div>
+
+        <div className="rounded-md mt-10 p-5 gap-10 shadow-sm bg-[#0c0e12]  grid grid-cols-4 place-content-center place-items-center ">
+          <div className="flex justify-center items-center flex-col  p-2">
+            <img className="w-5 h-5" src="/harmburger.png" alt="" />
+            <h5 className="capitalize ">all</h5>
+          </div>
+          <div className="flex justify-center items-center flex-col  p-2">
+            <img className="w-5 h-5" src="/one.png" alt="" />
+            <h5 className="capitalize ">planning</h5>
+          </div>
+          <div className="flex justify-center items-center flex-col  p-2">
+            <img className="w-5 h-5" src="/two.png" alt="" />
+            <h5 className="capitalize ">all</h5>
+          </div>
+          <div className="flex justify-center items-center flex-col  p-2">
+            <img className="w-5 h-5" src="/three.png" alt="" />
+            <h5 className="capitalize ">all</h5>
+          </div>
+        </div>
+
+        {/* Features */}
+        <div className="rounded-md mt-10 p-5 gap-10 shadow-sm bg-[#0c0e12]  grid grid-cols-4 place-content-center place-items-center">
+          <h3>Favourites</h3>
+
+          <div>
+            <div>
+              <span>
+                <img src="" alt="" />
+              </span>
+              <h5>Professional Emails</h5>
+            </div>
+
+            <p>Create and manage professional communication..</p>
           </div>
         </div>
       </header>
